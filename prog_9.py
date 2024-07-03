91a5b83786d4de29c88775ed4e6586716268ab53
abhishek-offical <145251001+abhishek-offical@users.noreply.github.com>
2024-07-03 10:04:13 +0000

    Delete prog_9.py

--- prog_9.py
+++
@@ -1,8 +0,0 @@
-# Create a program that stops printing numbers when it encounters a number greater than 5 using the
-# break statement.
-
-for i in range(1,11):
-    if i>5:   # skip all number greater then 5
-        continue
-    else:
-        print(i)
