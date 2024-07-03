504f3483e15917a9ec1c2ac17e187eadb1221f37
abhishek-offical <145251001+abhishek-offical@users.noreply.github.com>
2024-07-03 15:26:23 +0530

    Add files via upload

---
+++ day-1/prog_5.py
@@ -0,0 +1,11 @@
+# Create a program that checks if a given year is a leap year.
+
+year = int(input("Enter a year:"))
+
+if (year % 400 ==0) and (year % 100 ==0):
+    print(f"{year} is a leap year")
+elif(year % 4 ==0) and (year % 100 !=0):
+    print(f"{year} is a leap year")
+else:
+    print(f"{year} is not a leap year")
+    
---
+++ prog_2.py
@@ -0,0 +1,7 @@
+# Create variables to store your name, age, and favorite hobby. Print these variables
+
+name = "Abhishek kumar"
+age = 21 
+fav_hobby = "Exploring new things"
+
+print(f"Hello, I am {name} and my age is {age} and my hobby is {fav_hobby}")
