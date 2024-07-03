37f7951be3cdedbdb6ab57f03816f6cfbc2c7e65
abhishek-offical <145251001+abhishek-offical@users.noreply.github.com>
2024-07-03 10:02:18 +0000

    Delete prog_10.py

--- prog_10.py
+++
@@ -1,9 +0,0 @@
-# Define a function called greet that takes a name as an argument and prints "Hello, [name]!".
-
-def greet (name):
-    print(f"Hello, {name}!")
-
-
-name = "Abhishek Kumar"
-
-greet(name)
